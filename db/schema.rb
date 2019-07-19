571729cfeec321802409b8deb12043d368b99ebb
hmitani <a1941hm@aiit.ac.jp>
2019-07-19 07:24:16 +0000

    modify db/csv format

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_18_165436) do
+ActiveRecord::Schema.define(version: 2019_07_19_070550) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,12 @@ ActiveRecord::Schema.define(version: 2019_07_18_165436) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
+  end
+
+  create_table "weeks", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,23 +2,48 @@
 
 require "csv"
 
-CSV.foreach('db/lecture.csv') do |row|
-  Lecture.create(:id => row[0], :name => row[1], :quarter => row[2])
-end
-
-CSV.foreach('db/lecture_period.csv') do |row|
-  LecturePeriod.create(:lecture_id => row[0], :day_of_the_week => row[1], :period => row[2])
-end
-
 CSV.foreach('db/cource.csv') do |row|
   Cource.create(:id => row[0], :name => row[1])
 end
 
-CSV.foreach('db/cource_lecture.csv') do |row|
-  CourceLecture.create(:cource_id => row[0], :lecture_id => row[1])
+CSV.foreach('db/week.csv') do |row|
+  Week.create(:id => row[0], :name => row[1])
 end
 
-# CSV.foreach('db/user_lecture.csv') do |row|
-#   Cource.create(:user_id => row[0], :lecture_id => row[1])
-# end
+lecture_id = 1
+CSV.foreach('db/lecture.csv') do |row|
+  lecture_name = row[0]
+  quarter = row[1]
+  Lecture.create(:id => lecture_id, :name => lecture_name, :quarter => quarter)
+
+  lecture_count = row[2].to_i
+
+  column = 3
+  count = 0
+  while count < lecture_count do
+    week = row[column + 0]
+    column += 1
+    period = row[column + 1]
+    column += 1
+    LecturePeriod.create(:lecture_id => lecture_id, :day_of_the_week => week, :period => period)
+    count += 1
+  end
+
+  cource_count = row[column].to_i
+  column += 1
+
+  count = 0
+  while count < cource_count do
+    cource_id = row[column]
+    CourceLecture.create(:cource_id => cource_id, :lecture_id => lecture_id)
+    count += 1
+    column += 1
+  end
+
+  lecture_id += 1
+end
+
+#CSV.foreach('db/user_lecture.csv') do |row|
+#  Cource.create(:user_id => row[0], :lecture_id => row[1])
+#end
 
