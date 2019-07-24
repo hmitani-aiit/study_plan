6f79a904df26227d119a490a00e69b1a76b5b347
hmitani <a1941hm@aiit.ac.jp>
2019-07-24 07:27:50 +0000

    modify lecture controller and view

--- app/controllers/lectures_controller.rb
+++ app/controllers/lectures_controller.rb
@@ -1,5 +1,15 @@
 class LecturesController < ApplicationController
   def index 
-    @lectures = [{name: "国語"},{name: "算数"}, {name: "理科"}, {name: "社会"}]
+    @lectures = Lecture
+        .select(:id, :name, :quarter)
+        .order(:quarter)
+
+    @periods = []
+    @lectures.each do | lecture |
+        @periods[lecture.id] = LecturePeriod
+            .select(:name, :period)
+            .joins("INNER JOIN weeks ON lecture_periods.day_of_the_week = weeks.id")
+            .where(lecture_id: lecture.id)
+    end
   end
 end
