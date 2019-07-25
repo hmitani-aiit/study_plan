d4028130452ac1a85db975afc6b7ad61ab250f38
Shingo Akagaki <a1901sa@aiit.ac.jp>
2019-07-26 03:02:28 +0900

    refactoring the model and controll

--- app/models/lecture_period.rb
+++ app/models/lecture_period.rb
@@ -1,2 +1,4 @@
 class LecturePeriod < ApplicationRecord
+    belongs_to :lecture
+    has_one :week, primary_key: :day_of_the_week, foreign_key: :id
 end
--- app/models/week.rb
+++ app/models/week.rb
@@ -1,2 +1,3 @@
 class Week < ApplicationRecord
+    has_many :lecture_period, primary_key: :id
 end
