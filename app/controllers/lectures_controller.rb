1e4255f8ac3e64bb481771e92b6453bbde25eef9
Shingo Akagaki <a1901sa@aiit.ac.jp>
2019-07-26 19:26:01 +0900

    connect user

--- app/controllers/lectures_controller.rb
+++ app/controllers/lectures_controller.rb
@@ -3,5 +3,6 @@ class LecturesController < ApplicationController
     @lectures = Lecture
         .includes([:lecture_period, :cource])
         .order(:quarter)
+    @current_user = User.find_by(id: session[:user_id])
   end
 end
