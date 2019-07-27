b97002ae5fc4ad25bca33277c84ff6ac703377e0
Shingo Akagaki <a1901sa@aiit.ac.jp>
2019-07-27 22:06:43 +0900

    initial_state

--- app/controllers/lectures_controller.rb
+++ app/controllers/lectures_controller.rb
@@ -1,8 +1,19 @@
 class LecturesController < ApplicationController
   def index 
     @current_user = User.includes(:lecture).find_by(id: session[:user_id])
+=begin
+This query should be written with "LEFT OUTER JOIN ... ON ..." with "AND", but I will belive in psqls execution plan.
+SELECT "lectures".*, user_lectures.user_id FROM "lectures"
+    LEFT OUTER JOIN "user_lectures"
+        ON "user_lectures"."lecture_id" = "lectures"."id"
+    WHERE ("user_lectures"."user_id" = $1 OR "user_lectures"."user_id" IS NULL)
+    ORDER BY "lectures"."quarter" ASC
+=end
     @lectures = Lecture
-        .includes([:lecture_period, :cource])
+        .includes([:week, :cource])
+        .select("lectures.*, user_lectures.user_id")
+        .left_joins(:user_lecture)
+        .where(user_lectures: {user_id: [session[:user_id],nil]})
         .order(:quarter)
   end
 end
--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -1,5 +1,6 @@
 class Lecture < ApplicationRecord
     has_many :lecture_period
+    has_many :week, through: :lecture_period
     has_many :cource_lecture
     has_many :cource, through: :cource_lecture
     has_many :user_lecture
