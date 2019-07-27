85894f993e9a1e8d711a207f5009ffb1e5176ae8
sasayaka <a1927sd@aiit.ac.jp>
2019-07-27 00:10:37 +0000

    nologin noview

--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -1,4 +1,6 @@
 class Lecture < ApplicationRecord
+#  belongs_to :user
+#    validates :user_id, presence: true
     has_many :lecture_period
     has_many :cource_lecture
     has_many :cource, through: :cource_lecture
